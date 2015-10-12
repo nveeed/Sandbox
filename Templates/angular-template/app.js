e30e0d85720934cc3c89bcebff4de23fed870576
Naveed <08es34@gmail.com>
2015-10-12 11:20:52 +0500

    no message

--- Templates/angular-template/app.js
+++ Templates/angular-template/app.js
@@ -3,13 +3,11 @@
 angular.module('myApp', [
     'ngRoute',
     'ngAnimate',
-    'Home',
-	'AboutUs'// append more modules here
+    'Home'// append more modules here
 ]).
 config(['$routeProvider', function($routeProvider) {
     $routeProvider.otherwise({redirectTo: '/home'});
 }]);
 
 // @codekit-append "pages/home/home.js"
-// @codekit-append "pages/about-us/about-us.js"
 // append more files here
