c7bc2255e05c281c487c5f62f8965ffc869f388b
Naveed <08es34@gmail.com>
2016-04-27 18:41:33 +0500

    objects are passed by reference in php by default

--- PHP/object-pass-by-ref-val.php
+++ PHP/object-pass-by-ref-val.php
@@ -4,3 +4,36 @@
  * Date: 4/27/16
  * Time: 6:33 PM
  */
+
+class Person
+{
+
+    public $name;
+    public $age;
+
+    public function __construct($name,$age)
+    {
+        $this->name = $name;
+        $this->age = $age;
+    }
+
+}
+
+function incAge($person)
+{
+    $person->age ++;
+}
+
+function incNum($num)
+{
+    $num ++;
+}
+
+$p1 = new Person('naveed', 26);
+
+echo($p1->age); // 26
+incAge($p1);
+echo($p1->age); // 27
+$num = 9;
+incNum($num);
+echo $num; // 9
